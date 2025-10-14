333e27ff40f7f37a4ee8505491e0ea17e723bf82
Cain Tapp <cainlucastapp@gmail.com>
2025-10-14 10:44:21 -0700

    basic structure for HTML and API fetch

---
+++ index.js
@@ -0,0 +1,14 @@
+//API 
+//https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+//As an example, to get definition of English word hello, you can send request to
+//https://api.dictionaryapi.dev/api/v2/entries/en/hello 
+
+const dictionaryApi = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const word = "hello";
+
+
+const request = dictionaryApi + word;
+
+
+console.log(request);
