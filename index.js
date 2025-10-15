df7825991bc4ea1c5de74c74c5670a0394b8b9dd
Cain Tapp <cainlucastapp@gmail.com>
2025-10-14 17:02:01 -0700

    basic fucntion working for API and testing

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-//API 
-//https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+//API: https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 //As an example, to get definition of English word hello, you can send request to
 //https://api.dictionaryapi.dev/api/v2/entries/en/hello 
-
 const dictionaryApi = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-const word = "hello";
+
+//Form elements
+const wordInput = document.getElementById("word-input");
+//Submit Button
+const fetchDefinition = document.getElementById("fetch-definition");
 
 
-const request = dictionaryApi + word;
+//Submit form
+fetchDefinition.addEventListener("click", () => {
+    //Get form value
+    const word = wordInput.value;
+    //Fetch weather alerts
+    fetchWord(word);
+    //Clear the input field after submit
+    wordInput.value = "";
+});
 
 
-console.log(request);
+//Fetch word
+async function fetchWord(wordInput) {
+    //API Request
+    const request = dictionaryApi + wordInput
+
+    //fetch
+    try {
+        //API request
+        const response = await fetch(dictionaryApi + wordInput);
+        const data = await response.json();
+        console.log(data); 
+        const entry = data[0]; 
+        console.log(entry.word); 
+        return entry.word;
+    } catch (error) {
+        console.error('Error fetching post data:', error);
+    }
+
+}
 
 
 
-
+// Export for testing
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { fetchWord };
+}
--- planning.js
+++ planning.js
@@ -1,1 +1,42 @@
 //Psuedocode
+
+
+//Fetch API
+const dictionaryApi = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+
+
+//Form elements
+//const wordInput
+//const fetchDefinition <--Submit Button
+
+//Display variables
+//const errorMessages
+//const definition
+
+
+//Submit form Click Event Listner
+    //Get form vlaue: word = input.value
+    //call function for fetchWord(wordInput) 
+    //clear input field after submit
+//end
+
+
+//function fetchWord(wordInput)
+    //Search functionality, for a word.
+    //API Request
+    //Fetch Request
+        //then if ok return json if not ok throw error
+        //then build elements
+        //catch errors
+    //end
+//end
+
+
+
+
+
+    //Displaying pronunciation, definitions, and synonyms.
+    //Providing audio playback for pronunciation.
+
+
